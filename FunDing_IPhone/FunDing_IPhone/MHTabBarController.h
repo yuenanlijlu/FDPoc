fd14cf1d7c8ecb9b5f2b2bcaeba5795886de59ba
yuenanlijlu <yuenanl@gmail.com>
2012-11-10 08:22:08 +0100

    add views

---
+++ FunDing_IPhone/FunDing_IPhone/MHTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  MHTabBarController.h
+//  FunDing_IPhone
+//
+//  Created by Ray on 11/10/12.
+//  Copyright (c) 2012 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MHTabBarController : UIViewController
+
+@end
---
+++ FunDing_IPhone/FunDing_IPhone/MHTabBarController.m
@@ -0,0 +1,13 @@
+//
+//  MHTabBarController.m
+//  FunDing_IPhone
+//
+//  Created by Ray on 11/10/12.
+//  Copyright (c) 2012 apple. All rights reserved.
+//
+
+#import "MHTabBarController.h"
+
+@implementation MHTabBarController
+
+@end
